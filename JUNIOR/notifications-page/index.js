382f38501e76261a17a35a30c035fe3c9faf6c29
thomas-x-69 <thomas.ashraf975@gmail.com>
2023-03-10 00:53:35 +0200

    first junior challenge (finished)

--- JUNIOR/notifications-page/index.js
+++ JUNIOR/notifications-page/index.js
@@ -1,50 +1,62 @@
 //todo: variables
-
-//todo: when page loaded check the number of notifications (function) , if counter == 0 do the unread function
-
-//todo: when clicks on notification :remove unread Class - remove background - remove red sign - update counter
-
-/* 
-todo: when clicks readAll
-if has allRead class : go throught all notification & avtivate them
-else if has not : go throught all notification & deAvtivate them
-*/
-
 let markAll = document.getElementById("mark-all");
 let row = document.getElementsByClassName("row");
 let ToRead = document.getElementsByClassName("to-read");
-let divs = document.querySelectorAll("section div.row");
-let counter = document.getElementById("notifications-counter");
+let counterElement = document.getElementById("notifications-counter");
 let section = document.getElementById("sections");
-for (i = 0; i <= section.length; i++) {
-  if (section[i].classList.contains("to-read")) {
-    counter++;
-    i++;
-  } else continue;
+checkNumber();
+//todo: when page loaded check the number of notifications (function) , if counter == 0 do the unread function
+function checkNumber() {
+  let counter = 0;
+  var children = section.children;
+  for (var i = 0; i < children.length; i++) {
+    if (children[i].classList.contains("to-read")) {
+      counter++;
+    }
+  }
+  console.log(`notification number now is : [${counter}]`);
+  counterElement.innerHTML = counter;
+  if (counterElement.innerHTML == 0) {
+    markAll.classList.add("unMark");
+    markAll.innerHTML = "unMark all";
+  } else {
+    markAll.classList.remove("unMark");
+    markAll.innerHTML = "Mark all as read";
+  }
 }
-console.log(counter.innerHTML);
-// if object has to read then
+//todo: when clicks on notification :remove unread Class - remove background - update counter
 for (let oneRow of row) {
   oneRow.addEventListener("click", (e) => {
     if (e.target.classList.contains("to-read")) {
       e.target.classList.remove("to-read");
-    }
+      checkNumber();
+    } else return 0;
   });
 }
+/*
+todo: when clicks readAll
+if has allRead class : go throught all notification & axtivate them
+else if has not : go throught all notification & deAvtivate them
+*/
+
 markAll.addEventListener("click", (e) => {
-  for (let div of divs) {
-    if (e.target.classList.contains("unMark")) {
-      if (!div.classList.contains("to-read")) {
-        div.classList.add("to-read");
-        e.target.classList.remove("unMark");
-        e.target.innerHTML = "Mark all as read";
+  if (!markAll.classList.contains("unMark")) {
+    markAll.classList.add("unMark");
+    markAll.innerHTML = "unMark all";
+    for (let singleRow of row) {
+      if (singleRow.classList.contains("to-read")) {
+        singleRow.classList.remove("to-read");
       }
-    } else if (div.classList.contains("to-read")) {
+      checkNumber();
+    }
+  } else if (markAll.classList.contains("unMark")) {
+    markAll.classList.remove("unMark");
+    markAll.innerHTML = "Mark all as read";
+    for (let singleRow of row) {
+      if (!singleRow.classList.contains("to-read")) {
+        singleRow.classList.add("to-read");
+      }
+      checkNumber();
     }
   }
 });
-if (counter.innerHTML == "0") {
-  document.body.div.classList.remove("to-read");
-  markAll.classList.add("unMark");
-  markAll.innerHTML = "unMark all";
-}
